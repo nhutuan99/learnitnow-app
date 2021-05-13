399f8ad593a299ccffb0be28dddc4b43906f9247
nhutuan99 <66738885+nhutuan99@users.noreply.github.com>
2021-05-13 23:23:36 +0700

    Update LoginForm

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { useState, useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import AlertMessage from '../layout/AlertMessage';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LoginForm = () => {
 	// Context
